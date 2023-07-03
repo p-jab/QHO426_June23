0c617794827c355dd14942eb7a7adc8b744fb7ee
p-jab <piotr.bednorz@qa.com>
2023-07-03 10:12:49 +0100

    Week 1 Demos

---
+++ basics/input/string_operators.py
@@ -0,0 +1,8 @@
+lives = int(input("Please enter number of lives: "))
+energy = int(input("Please enter energy level: "))
+shield = int(input("Please enter shield level: "))
+
+print("Health has been set")
+print("Lives: " + "\u2764"*lives)
+print("Energy: " + "\u2756"*energy)
+print("Shield: " + "\u2742"*shield)
---
+++ demos/calculator.py
@@ -0,0 +1,7 @@
+n1 = float(input("Enter first number: "))
+n2 = float(input("Enter second number: "))
+print(f"{n1} + {n2} = {n1+n2}")
+print("{} - {} = {}".format(n1, n2, n1-n2))
+print(str(n1) + " x " + str(n2) + " = " + str(n1*n2))
+print(n1,"/",n2,"=", n1/n2)
+print(f"{n1}^{n2} = {n1**n2}")
